01aeb7ce0257116f8c27a2b6bf096853613705b4
Elliott Thompson <elliott@moo.com>
2016-11-20 21:30:23 +0000

    added error handling for the servers api endpoints

--- server/api/blogposts.js
+++ server/api/blogposts.js
@@ -1,7 +1,11 @@
 module.exports = function(app, db) {
     app.get('/api/blogposts', function(req, res) {
-        db.blogposts(function(json) {
-            res.json(json);
+        db.blogposts(function(result) {
+            if (result == 500) {
+                res.status(500).send('Looks like something has broken!');
+            } else {
+                res.json(result);
+            }
         });
     });
 };
--- server/api/homepage.js
+++ server/api/homepage.js
@@ -1,7 +1,11 @@
 module.exports = function(app, db) {
     app.get('/api/homepage', function(req, res) {
-        db.homepage(function(json) {
-            res.json(json);
+        db.homepage(function(result) {
+            if (result === 500) {
+                res.status(500).send('Looks like something has broken!');
+            } else {
+                res.json(result);
+            }
         });
     });
 };
